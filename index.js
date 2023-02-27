8880d2ca2c2793bef73acbf7af101305d32935d1
NikitaShevchuk <shevchuk.nikita.gh@gmail.com>
2023-02-27 04:24:20 +0200

    change timeout

--- index.js
+++ index.js
@@ -19,17 +19,15 @@ const start = async () => {
                         // if (index > 10) return true;
 
                         await new Promise((resolve) => {
-                            setTimeout(resolve, 500 * index);
+                            setTimeout(resolve, 1000 * index);
                         });
                         const songInfo = await lyricsScrapper(diff.link);
 
                         if (!songInfo) return true;
 
-                        // const { added } = await addNewSong(songInfo);
-                        // if (added) addedCount++;
-                        // return added;
-
-                        return true;
+                        const { added } = await addNewSong(songInfo);
+                        if (added) addedCount++;
+                        return added;
                     })
                 );
             } catch (error) {
